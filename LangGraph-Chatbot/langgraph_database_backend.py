6737e62de81a4cd9662c3346f4eb6945a73076dc
shivanshgupta2504 <shivanshgupta2057@gmail.com>
2026-01-08 20:23:24 +0530

    Updated chatbot

---
+++ LangGraph-Chatbot/langgraph_database_backend.py
@@ -0,0 +1,39 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated, List
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+llm = ChatOpenAI(model='gpt-4o-mini')
+
+class ChatState(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {'messages': [response]}
+
+# Create the sqlite3 database
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config.get("configurable", {}).get("thread_id", ""))
+    return list(all_threads)
