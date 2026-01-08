3317e45bc51f5ebd8f65b3b791d8966e430bdf64
shivanshgupta2504 <shivanshgupta2057@gmail.com>
2026-01-08 14:01:05 +0530

    added persistance and Streamlit Chatbot

---
+++ LangGraph-Chatbot/streamlit_frontend.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG = {"configurable": {"thread_id": "1"}}
+
+# st.session_state is itself a dictionary which does not change once whole script runs again
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# Loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input("Type here...")
+
+if user_input:
+    # First add the message to message history
+    st.session_state['message_history'].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+
+
+    response = chatbot.invoke(
+        {"messages": [HumanMessage(content=user_input)]},
+        config=CONFIG
+    )
+    ai_message = response["messages"][-1].content
+    # First add the message to message history
+    st.session_state['message_history'].append({"role": "assistant", "content": ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message)
